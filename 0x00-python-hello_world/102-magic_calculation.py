3d9cf5a8cff6f33d8fa8d5e3a490e95515ee8608
Stevecmd <murimi101@gmail.com>
2024-03-11 13:21:04 +0300

    test 11

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,23 +1,5 @@
 #!/usr/bin/python3
+
 def magic_calculation(a, b):
-	"""
-	magic_calculation - Performs a magical calculation based on given inputs.
-
-	This function calculates the result of a magical operation using two input parameters 'a' and 'b'.
-	It first initializes the result to a default value of 98. Then, it computes the power of 'a' raised to 'b'
-	and adds it to the initialized result. Finally, it returns the final calculated result.
-
-	Args:
-		a (int): The first input parameter.
-		b (int): The second input parameter.
-
-	Returns:
-		int: The result of the magical calculation.
-		use "print(magic_calculation(2, 3))" to see output
-	"""
-
-	result = 98
-	power_result = a ** b
-
-	final_result = result + power_result
-	return final_result
+    result = (98 + a ** b)
+    return result
