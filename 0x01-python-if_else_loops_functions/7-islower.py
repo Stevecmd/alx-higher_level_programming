85ba9f6bbdab44918a63ab4df7bfd650491ede7b
Stevecmd <murimi101@gmail.com>
2024-03-12 12:15:46 +0300

    converted indentation to spaces as per pycode style

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 
 def islower(c):
-	"""
-	Check if a character is lowercase.
+    """
+    Check if a character is lowercase.
 
-	Args:
-		c: A character (string of length 1).
+    Args:
+        c: A character (string of length 1).
 
-	Returns:
-		True if the character is lowercase, False otherwise.
-	"""
-	if 'a' <= c <= 'z':
-		return True
-	else:
-		return False
+    Returns:
+        True if the character is lowercase, False otherwise.
+    """
+    if 'a' <= c <= 'z':
+        return True
+    else:
+        return False
