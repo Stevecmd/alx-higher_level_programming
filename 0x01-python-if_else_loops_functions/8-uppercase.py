d0194df612f6e0007f6c75dcadfba03e39867dcc
Stevecmd <murimi101@gmail.com>
2024-03-12 13:42:40 +0300

    checker failing

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,6 @@
 # Author - Steve
 
 def uppercase(str):
-    """Convert and print a string of characters in uppercase."""
     for c in str:
         if ord(c) >= 97 and ord(c) <= 122:
             c = chr(ord(c) - 32)
