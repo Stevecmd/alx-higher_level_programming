8f4f8572da8f98e89c3f9f7e01702db35eddc8f8
Stevecmd <murimi101@gmail.com>
2024-03-12 13:18:45 +0300

    fixing checker errors

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -2,4 +2,4 @@
 
 def pow(a, b):
     """Computes a to the power of b and returns the value"""
-    return (a * b)
+    return (a ** b)
--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 
 def fizzbuzz():
     result = []
-    
+
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
             result.append('FizzBuzz')
