76e3c8e5208f4d6019a63087432a58a4baa28235
Stevecmd <murimi101@gmail.com>
2024-03-14 12:37:52 +0300

    0-add.py update 2

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 
+"""Importing the add function from add_0 module"""
+from add_0 import add
+
+"""Checking if the script is executed directly"""
 if __name__ == "__main__":
-    # Importing the add function from add_0 module
-    from add_0 import add
     
-    # Initializing variables
+    """Initializing variables"""
     a = 1
     b = 2
     
-    # Printing the result of addition
+    """Printing the result of addition"""
     print("{} + {} = {}".format(a, b, add(a, b)))
