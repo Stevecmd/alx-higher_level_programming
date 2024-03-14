7b7590e47f0be3eccc6c733d6d35a277098cc4d3
Stevecmd <murimi101@gmail.com>
2024-03-14 12:49:33 +0300

    test 3

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python3
 
-# If the script is executed directly
 if __name__ == "__main__":
-    # Importing the add, sub, mul, div functions from calculator_1 module
+    """Does calculations"""
     from calculator_1 import add, sub, mul, div
 
-    # Initializing variables
     a = 10
     b = 5
 
-    # Performing addition and printing the result
     print("{} + {} = {}".format(a, b, add(a, b)))
 
-    # Performing subtraction and printing the result
     print("{} - {} = {}".format(a, b, sub(a, b)))
 
-    # Performing multiplication and printing the result
     print("{} * {} = {}".format(a, b, mul(a, b)))
 
-    # Performing division and printing the result
     print("{} / {} = {}".format(a, b, div(a, b)))
