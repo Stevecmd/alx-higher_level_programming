ec5c62b1be414af9ed8cd12ff3bb9e244d84681c
Stevecmd <murimi101@gmail.com>
2024-03-16 12:31:32 +0300

    Shortened descriptive message

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    """Retrieves the element at the specified index in list"""
-    if idx < 0 or idx >= len(my_list):
+    """Retrieves the element at index in list"""
+    if (idx < 0) or idx >= len(my_list):
         return None
     return(my_list[idx])
