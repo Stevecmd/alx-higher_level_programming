e24d2a71017c37874bf28e7131cc6cc12d812102
Stevecmd <murimi101@gmail.com>
2024-03-16 12:10:01 +0300

    Fixed EOF NL as per pycode style

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,7 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     """Add two tuples."""
-    """Ensure both tuples have at least 2 elements by padding with zeros if necessary"""
+    """Ensure both tuples have at least 2 elements default 0"""
     tuple_a += (0, 0)
     tuple_b += (0, 0)
-    """Return a tuple with the sum of corresponding elements from tuple_a and tuple_b"""
+    """Return a tuple with the sum of corresponding elements"""
     return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
