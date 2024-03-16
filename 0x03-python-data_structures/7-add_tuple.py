b602d465f5a8e0ca3f5182ce13317c8b9d4adc65
Stevecmd <murimi101@gmail.com>
2024-03-16 12:18:51 +0300

    Added an extra condition to meet checker requirements

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,7 @@
 def add_tuple(tuple_a=(), tuple_b=()):
-    """Add two tuples."""
-    """Ensure both tuples have at least 2 elements default 0"""
-    tuple_a += (0, 0)
-    tuple_b += (0, 0)
+    """Add two tuples.
+        Ensure both tuples have at least 2 elements default 0"""
+    tuple_a = tuple_a[:2] + (0, 0) if len(tuple_a) < 2 else tuple_a[:2]
+    tuple_b = tuple_b[:2] + (0, 0) if len(tuple_b) < 2 else tuple_b[:2]
     """Return a tuple with the sum of corresponding elements"""
     return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
