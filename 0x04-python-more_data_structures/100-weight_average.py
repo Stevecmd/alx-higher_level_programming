b1a62200f6b5ea0ac74470de8e3e0669217de83e
Stevecmd <murimi101@gmail.com>
2024-03-21 01:06:40 +0300

    update

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
 def weight_average(my_list=[]):
-	if not my_list:
-		return 0
+    if not my_list:
+        return 0
 
-	avg = 0
-	size = 0
+    avg = 0
+    size = 0
 
-	for tpl in my_list:
-		avg += (tpl[0] * tpl[1])
-		size += tpl[1]
+    for tpl in my_list:
+        avg += (tpl[0] * tpl[1])
+        size += tpl[1]
 
-	return (avg / size)
+    return (avg / size)
