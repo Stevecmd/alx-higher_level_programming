a2b3bf1eec03fd8d7e9518c514175b49b0f8cafb
Stevecmd <murimi101@gmail.com>
2024-03-21 01:10:07 +0300

    update

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-	""" Convert roman string to digits
-	"""
-	if not isinstance(roman_string, str) or roman_string is None:
-		return 0
+    """ Convert roman string to digits
+    """
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
 
-	roman_numerals = {
-		"I": 1,
-		"V": 5,
-		"X": 10,
-		"L": 50,
-		"C": 100,
-		"D": 500,
-		"M": 1000
-	}
-	integer_value = 0
+    r_numerals = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+    integer_value = 0
 
-	for i in range(len(roman_string)):
-		if roman_numerals.get(roman_string[i], 0) == 0:
-			return 0
+    for i in range(len(roman_string)):
+        if r_numerals.get(roman_string[i], 0) == 0:
+            return 0
 
-		if (i != len(roman_string) - 1 and
-				roman_numerals[roman_string[i]] < roman_numerals[roman_string[i + 1]]):
-			integer_value -= roman_numerals[roman_string[i]]
-		else:
-			integer_value += roman_numerals[roman_string[i]]
+        if (i != len(roman_string) - 1 and
+                r_numerals[roman_string[i]] < r_numerals[roman_string[i + 1]]):
+            integer_value -= r_numerals[roman_string[i]]
+        else:
+            integer_value += r_numerals[roman_string[i]]
 
-	return integer_value
+    return integer_value
