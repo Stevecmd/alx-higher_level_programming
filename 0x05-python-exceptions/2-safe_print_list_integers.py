98b8454ce223d1df0662879d3642114baed16697
Stevecmd <murimi101@gmail.com>
2024-04-02 12:42:25 +0300

    removed empty space

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@
 def safe_print_list_integers(my_list=[], x=0):
     """Print the first x integers from a list."""
     ret = 0
-    
+
     for i in range(0, x):
         try:
             print("{:d}".format(my_list[i]), end="")
