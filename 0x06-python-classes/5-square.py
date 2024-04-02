fd8fb899779eaf3ed4510ceeaa456f956447a292
Stevecmd <murimi101@gmail.com>
2024-04-02 13:41:22 +0300

    update

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -18,16 +18,11 @@ class Square:
 
     @property
     def size(self):
-        """Get the current size of the square."""
+        """Get/set the current size of the square."""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Set the size of the square.
-
-        Args:
-            value (int): The new size of the square.
-        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -36,16 +31,11 @@ class Square:
 
     @property
     def position(self):
-        """Get the current position of the square."""
+        """Get/set the current position of the square."""
         return self.__position
 
     @position.setter
     def position(self, value):
-        """Set the position of the square.
-
-        Args:
-            value (tuple): The new position of the square.
-        """
         if not isinstance(value, tuple) or len(value) != 2:
             raise TypeError("position must be a tuple of 2 integers")
         elif not all(isinstance(num, int) and num >= 0 for num in value):
@@ -53,13 +43,13 @@ class Square:
         self.__position = value
 
     def area(self):
-        """Calculate the area of the square."""
+        """Return the current area of the square."""
         return self.__size * self.__size
 
     def my_print(self):
-        """Print the square using the '#' character."""
+        """Print the square with the '#' character."""
         if self.__size == 0:
-            print()
+            print("")
             return
 
         for _ in range(self.__position[1]):
@@ -68,7 +58,3 @@ class Square:
         for _ in range(self.__size):
             print(" " * self.__position[0], end="")
             print("#" * self.__size)
-
-if __name__ == "__main__":
-    my_square = Square(3, (1, 2))
-    my_square.my_print()
