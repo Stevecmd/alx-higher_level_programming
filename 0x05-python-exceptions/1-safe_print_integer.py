6d7bb9f4eb82f31ff542350e39e01781d3fc99e9
Stevecmd <murimi101@gmail.com>
2024-04-02 12:08:47 +0300

    test

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -10,10 +10,12 @@ def safe_print_integer(value):
     Returns:
         bool: Returns True if the integer is printed successfully,
               Returns False if a TypeError or ValueError occurs
-                     (indicating that the provided value is not a valid integer).
     """
     try:
+        # Attempt to print the integer using "{:d}".format()
         print("{:d}".format(value))
+        # Return True if printing is successful
         return True
     except (TypeError, ValueError):
+        # Return False if TypeError or ValueError occurs
         return False
