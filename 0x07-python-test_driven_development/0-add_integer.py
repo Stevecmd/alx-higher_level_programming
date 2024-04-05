c4d1f4e438e6cb8829b1453722dbe44b020ac20f
Stevecmd <murimi101@gmail.com>
2024-04-05 09:28:32 +0300

    0-add_integer.py program and test update

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,21 +1,12 @@
 #!/usr/bin/python3
-"""Defines a function to add integers."""
-
-
-def add_integer(a, b=98):
-    """Return the integer addition of two numbers.
-
-    This function takes two arguments, a and b,
-    and returns their integer sum.
-
-    Args:
-        a (int or float): The first number to be added.
-        b (int or float, optional): The second number to
-        be added. Default is 98.
+"""Defines a function to add integers.
 
     Raises:
         TypeError: If either a or b is not an integer.
     """
+
+
+def add_integer(a, b=98):
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
--- 0x07-python-test_driven_development/README.md
+++ 0x07-python-test_driven_development/README.md
@@ -1,3 +1,23 @@
 # 0x07. Python - Test-driven development 
 
 - Based on the requirements of each task, you should always write the documentation (module(s) + function(s)) and tests first, before you actually code anything
+
+## To run the individual tests run: <br />
+1. `0-add_integer.py`
+
+    My test 1 :
+    ```sh
+        python3 -m doctest ./tests/0-add_integer.txt
+    ```
+    
+    My test 2 :
+    ```sh
+    python3 -m doctest -v ./tests/0-add_integer.txt | tail -2
+    17 passed and 0 failed.
+    Test passed.
+    ```
+    Official test to also check word count: 
+    ```sh
+    python3 -c 'print(__import__("0-add_integer").__doc__)' | wc -l
+    5
+    ```
