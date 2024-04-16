59e5a6cb328e13befaa17369331c8b8d00bb95a0
Stevecmd <murimi101@gmail.com>
2024-04-16 21:17:41 +0300

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,12 +5,15 @@ import importlib
 
 
 if __name__ == "__main__":
-    save_to_json_file = importlib.import_module('5-save_to_json_file').save_to_json_file
-    load_from_json_file = importlib.import_module('6-load_from_json_file').load_from_json_file
+    save_to_json_file = importlib.import_module(
+        '5-save_to_json_file').save_to_json_file
+    load_from_json_file = importlib.import_module(
+        '6-load_from_json_file').load_from_json_file
 
     try:
         items = load_from_json_file("add_item.json")
     except FileNotFoundError:
         items = []
+
     items.extend(sys.argv[1:])
     save_to_json_file(items, "add_item.json")
