aa6ef3352448b84e71e82d2b26e59e489635cce1
Stevecmd <murimi101@gmail.com>
2024-04-16 21:43:57 +0300

    10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -7,7 +7,7 @@ class Student:
     """
     Defines a student by age, first_name and last_name.
     """
-    def __init__(self, age, last_name, first_name):
+    def __init__(self, first_name, last_name, age):
         """
         Initializes a Student instance with first_name, last_name, and age.
 
@@ -17,9 +17,9 @@ class Student:
             first_name (str): The first name of the student.
 
         """
-        self.age = age
-        self.last_name = last_name
         self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self, attrs=None):
         """
@@ -31,14 +31,7 @@ class Student:
         Returns:
             dict: Dictionary representation of the Student instance.
         """
-        if attrs is None:
-            return {
-                "age": self.age,
-                "last_name": self.last_name,
-                "first_name": self.first_name
-            }
-        else:
-            return {
-                attr: getattr(self, attr) for attr in attrs
-                if hasattr(self, attr)
-            }
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
