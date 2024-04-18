6125885c20ac1f0ecfcb2da7e0fabeea26909240
Stevecmd <murimi101@gmail.com>
2024-04-18 21:45:07 +0300

    Initial setup of rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Definition of Rectangle class"""
+from models.base import Base
+
+class Rectangle(Base):
+    """Class Rectangle."""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initializing a new Rectangle"""
+        
