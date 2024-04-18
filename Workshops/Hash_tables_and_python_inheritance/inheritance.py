a144fabdce6dc6a601f7f0ad99ae5fa84e79a765
Stevecmd <murimi101@gmail.com>
2024-04-18 01:30:38 -0700

    hash functions

--- Workshops/Hash_tables_and_python_inheritance/inheritance.py
+++ Workshops/Hash_tables_and_python_inheritance/inheritance.py
@@ -16,37 +16,37 @@ Teacher:
 
 class Person:
 	def __init__(self, name, email, age=-1):
-		self.name = name
-		self.email = email
-		self.age = age
+	    self.name = name
+	    self.email = email
+	    self.age = age
 	
 	def get_details(self):
-		detail = f"My name is {self.name} and my email is {self.email}"
-		print (detail)
+            detail = f"My name is {self.name} and my email is {self.email}"
+            print (detail)
 
 	def get_email(self):
-		return self.email
+            return self.email
 
 class Student(Person):
-	def __init__(self, name, email, age, classRoom):
-		self.classRoom = classRoom
-		super().__init__(name, email, age)
+    def __init__(self, name, email, age, classRoom):
+        self.classRoom = classRoom
+        super().__init__(name, email, age)
 
-	def is_above_18(self):
-		if self.age == -1:
-			raise ValueError("The Age is not given")
-		return self.age > 18
+    def is_above_18(self):
+        if self.age == -1:
+            raise ValueError("The Age is not given")
+        return self.age > 18
 	
-	def get_details(self):
-		detail = f"My name is {self.name} and my email is {self.email} \
-		. I am also a student in {self.classRoom}."
-		print(detail)
-		return detail
+    def get_details(self):
+        detail = f"My name is {self.name} and my email is {self.email} \
+                . I am also a student in {self.classRoom}."
+        print(detail)
+        return detail
 	
-	def get_email(self):
-		if self.age > 18:
-			return super().get_email()
-		return -1
+    def get_email(self):
+        if self.age > 18:
+            return super().get_email()
+        return -1
 
 
 person1 = Person("Eva", "eva@gmail.com")
@@ -57,3 +57,8 @@ person2.get_details()
 stud1 = Student("Solo", "solo@gmail.com", 25, "B-class")
 stud1.get_details()
 print(stud1.is_above_18())
+stud1.get_details()
+stud2 = Student("ALX", "asdf@gmail.com", 8, "B")
+stud2.get_details()
+print("student 1 email is:", stud1.get_email())
+print("student 2 email:", stud2.get_email())
