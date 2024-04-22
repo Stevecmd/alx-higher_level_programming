3f01f7b1d4e160f294d707a41e554497de7fa50d
Stevecmd <murimi101@gmail.com>
2024-04-22 18:48:16 +0300

    6. __str__

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -89,3 +89,13 @@ class Rectangle(Base):
         """Prints the Rectangle instance with '#' characters"""
         for _ in range(self.height):
             print("#" * self.width)
+
+    def __str__(self):
+        """Override the __str__ method"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id,
+            self.x,
+            self.y,
+            self.width,
+            self.height
+        )
