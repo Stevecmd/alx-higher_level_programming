810c776efd34c39c792988875920c598a1377483
Stevecmd <murimi101@gmail.com>
2024-04-22 23:41:11 +0300

    test

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Defines a Square class that inherits from Rectangle"""
 from models.rectangle import Rectangle
 
 
@@ -16,40 +17,40 @@ class Square(Rectangle):
         """
         super().__init__(size, size, x, y, id)
 
-    @property
-    def size(self):
-        """Getter for size attribute"""
-        return self.width
+    # @property
+    # def size(self):
+    #     """Getter for size attribute"""
+    #     return self.width
 
-    @size.setter
-    def size(self, value):
-        """Setter for size attribute"""
-        self.width = value
-        self.height = value
+    # @size.setter
+    # def size(self, value):
+    #     """Setter for size attribute"""
+    #     self.width = value
+    #     self.height = value
 
-    def update(self, *args, **kwargs):
-        """Assign attributes to the instance"""
-        if args:
-            if len(args) > 0:
-                self.id = args[0]
-            if len(args) > 1:
-                self.size = args[1]
-            if len(args) > 2:
-                self.x = args[2]
-            if len(args) > 3:
-                self.y = args[3]
-        elif kwargs:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
+    # def update(self, *args, **kwargs):
+    #     """Assign attributes to the instance"""
+    #     if args:
+    #         if len(args) > 0:
+    #             self.id = args[0]
+    #         if len(args) > 1:
+    #             self.size = args[1]
+    #         if len(args) > 2:
+    #             self.x = args[2]
+    #         if len(args) > 3:
+    #             self.y = args[3]
+    #     elif kwargs:
+    #         for key, value in kwargs.items():
+    #             setattr(self, key, value)
 
-    def to_dictionary(self):
-        """Returns the dictionary representation of a Square"""
-        return {
-            'id': self.id,
-            'size': self.width,
-            'x': self.x,
-            'y': self.y
-        }
+    # def to_dictionary(self):
+    #     """Returns the dictionary representation of a Square"""
+    #     return {
+    #         'id': self.id,
+    #         'size': self.width,
+    #         'x': self.x,
+    #         'y': self.y
+    #     }
 
     def __str__(self):
         """Override the __str__ method"""
