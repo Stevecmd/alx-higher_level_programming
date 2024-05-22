9d55b978e748fb444154fe10828ff703ba7b5676
Stevecmd <murimi101@gmail.com>
2024-05-22 16:52:59 +0300

    minor update

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,7 +1,7 @@
 -- List all shows by their rating from the hbtn_0d_tvshows_rate database
 
 SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
-FROM tv_shows
-LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
+FROM hbtn_0d_tvshows.tv_shows
+LEFT JOIN hbtn_0d_tvshows.tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
 GROUP BY tv_shows.title
 ORDER BY rating DESC;
