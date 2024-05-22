b48bccca171c5ba440f365fe47db791f11954758
Stevecmd <murimi101@gmail.com>
2024-05-22 16:15:41 +0300

    fix

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -4,12 +4,11 @@
 -- Retrieve shows without the genre "Comedy"
 SELECT tv_shows.title
 FROM tv_shows
--- Left join with tv_shows_genres to find linked genres
-LEFT JOIN tv_shows_genres ON tv_shows.id = tv_shows_genres.show_id
+-- Left join with tv_show_genres to find linked genres
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 -- Left join with tv_genres to find genres associated with "Comedy"
-LEFT JOIN tv_genres ON tv_shows_genres.genre_id = tv_genres.id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id AND tv_genres.name = 'Comedy'
 -- Filter out shows with the genre "Comedy"
-WHERE tv_genres.name = 'Comedy'
-AND tv_genres.id IS NULL
+WHERE tv_genres.id IS NULL
 -- Order results by show title in ascending order
 ORDER BY tv_shows.title;
