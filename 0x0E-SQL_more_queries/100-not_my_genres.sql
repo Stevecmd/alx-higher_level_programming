3fee9a8732cbf014f3671c4d8f02ab0968c75e02
Stevecmd <murimi101@gmail.com>
2024-05-22 15:52:21 +0300

    100-not_my_genres.sql

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,19 @@
+-- Use the hbtn_0d_tvshows database to list all
+-- genres not linked to the show Dexter
+
+-- Select the genre names from the tv_genres table
+SELECT tv_genres.name
+FROM tv_genres
+
+-- Left join with tv_shows_genres to find linked genres
+LEFT JOIN tv_shows_genres ON tv_genres.id = tv_shows_genres.genre_id
+
+-- Left join with tv_shows to find genres associated with the show "Dexter"
+LEFT JOIN tv_shows ON tv_shows_genres.show_id = tv_shows.id
+
+-- Filter out genres linked to "Dexter"
+WHERE tv_shows.title = 'Dexter'
+AND tv_shows.id IS NULL
+
+-- Order results by genre name in ascending order
+ORDER BY tv_genres.name;
