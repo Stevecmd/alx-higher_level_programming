3e62821a1d6a1768753359bf3fad80af45dc297b
Stevecmd <murimi101@gmail.com>
2024-06-17 10:34:30 +0300

    1-multi_languages.js

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// Define constant variables with the required strings
+const firstLine = 'C is fun';
+const secondLine = 'Python is cool';
+const thirdLine = 'JavaScript is amazing';
+
+// Print each line to the console
+console.log(firstLine);
+console.log(secondLine);
+console.log(thirdLine);
