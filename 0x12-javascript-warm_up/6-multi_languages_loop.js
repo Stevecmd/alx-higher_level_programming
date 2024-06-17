b52b8e844793eaddeef372c629134cf4e1f3d823
Stevecmd <murimi101@gmail.com>
2024-06-17 11:09:03 +0300

    6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// Define an array of strings
+const lines = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+// Iterate through the array and print each line
+for (let i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
+}
