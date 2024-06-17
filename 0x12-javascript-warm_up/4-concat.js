b9a1a1bd68e2df8fa3dd8fa77cd61a386d47c12f
Stevecmd <murimi101@gmail.com>
2024-06-17 10:51:32 +0300

    4-concat.js

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// Get the arguments passed to the script
+const args = process.argv.slice(2);
+
+// Define variables to hold the arguments
+const arg1 = args[0] || "undefined";
+const arg2 = args[1] || "undefined";
+
+// Print the formatted output
+console.log(`${arg1} is ${arg2}`);
