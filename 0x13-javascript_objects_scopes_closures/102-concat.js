645018ed0560fb2933b060676809e00657ebd4e4
Stevecmd <murimi101@gmail.com>
2024-06-18 15:38:26 +0300

    test

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -7,4 +7,4 @@ const contentA = fs.readFileSync(process.argv[2]).toString();
 const contentB = fs.readFileSync(process.argv[3]).toString();
 
 // Write concatenated content to fileC
-fs.writeFileSync(process.argv[4], contentA + '\n' + contentB + '\n');
+fs.writeFileSync(process.argv[4], contentA + '\n' + contentB);
