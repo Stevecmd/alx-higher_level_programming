c4adc6be36cbb5a73d450d6ca26df1100e2347d5
Stevecmd <murimi101@gmail.com>
2024-06-21 13:30:10 +0300

    test

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,52 +1,58 @@
 #!/usr/bin/python3
 """
 1-filter_states module
-Lists all unique states with a name starting with 'N' from the database hbtn_0e_0_usa
+Lists all states with a name starting with 'N' from the database hbtn_0e_0_usa
 """
 import MySQLdb
 import sys
 
-
 def filter_states():
     """
     Connects to the MySQL database and lists
-    all unique states with a name starting with 'N'.
+    all states with a name starting with 'N'.
     """
     # Get MySQL credentials and database name from command-line arguments
+    if len(sys.argv) != 4:
+        print("Usage: ./1-filter_states.py <username> <password> <database>")
+        return
+    
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
-    # Connect to the MySQL server
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-    )
+    try:
+        # Connect to the MySQL server
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+        )
 
-    # Create a cursor object to interact with the database
-    cur = db.cursor()
+        # Create a cursor object to interact with the database
+        cur = db.cursor()
 
-    # Execute the SQL query to list unique states with names starting with 'N'
-    query = """
-    SELECT DISTINCT id, name
-    FROM states
-    WHERE name LIKE 'N%'
-    ORDER BY id ASC
-    """
-    cur.execute(query)
+        # Execute the SQL query to list states with names starting with 'N'
+        query = """
+        SELECT id, name
+        FROM states
+        WHERE name LIKE BINARY 'N%'
+        ORDER BY id ASC
+        """
+        cur.execute(query)
 
-    # Fetch all the results and print them
-    states = cur.fetchall()
-    for state in states:
-        print(state)
+        # Fetch all the results and print them
+        states = cur.fetchall()
+        for state in states:
+            print(state)
 
-    # Close the cursor and database connection
-    cur.close()
-    db.close()
+        # Close the cursor and database connection
+        cur.close()
+        db.close()
 
+    except MySQLdb.Error as e:
+        print(f"Error connecting to MySQL: {e}")
 
 if __name__ == "__main__":
     filter_states()
