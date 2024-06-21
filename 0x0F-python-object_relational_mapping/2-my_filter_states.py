a942a767f7a9dbb9834d3a062031e0dffed948f0
Stevecmd <murimi101@gmail.com>
2024-06-21 21:39:29 +0300

    test5

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,34 +9,26 @@ import MySQLdb
 
 
 if __name__ == "__main__":
-    # Check for correct number of arguments
-    if len(sys.argv) != 5:
-        print("Usage: ./2-my_filter_states.py <mysql username> "
-              "<mysql password> <database name> <state name>")
-        sys.exit(1)
-
-    # Assign command line arguments to variables
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    name_pattern = sys.argv[4]
 
     try:
         # Connect to the MySQL server
         db = MySQLdb.connect(
             host="localhost",
             port=3306,
-            user=username,
-            passwd=password,
-            db=database
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
         )
 
         # Create a cursor object to interact with the database
         cur = db.cursor()
 
         # Execute the SQL query with parameterized query
-        query = "SELECT * FROM states WHERE name LIKE BINARY %s"
-        cur.execute(query, (name_pattern,))
+        name_pattern = sys.argv[4]
+        query = (
+            "SELECT * FROM states WHERE name LIKE BINARY '{}'".format(name_pattern)
+        )
+        cur.execute(query)
 
         # Fetch all the results and print them
         states = cur.fetchall()
