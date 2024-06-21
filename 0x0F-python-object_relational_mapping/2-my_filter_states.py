dcc680d634ad0caa795e8a80c2a44f7044e362d0
Stevecmd <murimi101@gmail.com>
2024-06-21 21:23:41 +0300

    test

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -27,41 +27,7 @@ def filter_states(username, password, database, state_name):
         # Create a cursor object to interact with the database
         cur = db.cursor()
 
-        # Execute the SQL query to find states with the exact given name
-        query = """
-        SELECT *
-        FROM states
-        WHERE name = %s
-        ORDER BY id ASC
-        """
-        cur.execute(query, (state_name,))
-
-        # Fetch all the results and print them
-        states = cur.fetchall()
-        for state in states:
-            print(state)
-
-        # Close the cursor and database connection
-        cur.close()
-        db.close()
-
-    except MySQLdb.Error as e:
-        print(f"Error connecting to MySQL: {e}")
-        sys.exit(1)
-
-
-if __name__ == "__main__":
-    # Check for correct number of arguments
-    if len(sys.argv) != 5:
-        print("Usage: ./2-my_filter_states.py <mysql username> "
-              "<mysql password> <database name> <state name>")
-        sys.exit(1)
-
-    # Assign command line arguments to variables
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
-
-    # Call the function with provided arguments
-    filter_states(username, password, database, state_name)
+        cur.execute("SELECT * \
+                    FROM `states` \
+                    WHERE BINARY `name` = '{}'".format(sys.argv[4]))
+        [print(state) for state in cur.fetchall()]
