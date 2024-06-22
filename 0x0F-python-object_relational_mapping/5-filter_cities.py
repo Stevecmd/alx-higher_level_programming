c65e9a43e9994336947d0c744628d02ea6f990f6
Stevecmd <murimi101@gmail.com>
2024-06-22 11:35:36 +0300

    test

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -11,7 +11,15 @@ if __name__ == "__main__":
     Connects to the MySQL database and lists
     all cities of the specified state.
     """
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    # Get MySQL credentials,
+    db = db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+        )
     c = db.cursor()
     c.execute("SELECT * FROM `cities` as `c` \
                INNER JOIN `states` as `s` \
