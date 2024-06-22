80d8d787d80aeff67224e64cf57619cff528b4f7
Stevecmd <murimi101@gmail.com>
2024-06-22 11:11:46 +0300

    test

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -9,17 +9,28 @@ import sys
 
 if __name__ == "__main__":
     # Get arguments
-    username, password, database, state_name = argv[1:5]
+    if len(sys.argv) != 5:
+        print("Usage: ./5-filter_cities.py <username> \
+                <password> <database> <state_name>")
+        sys.exit(1)
+
+    username, password, database, state_name = sys.argv[1:5]
+
+    # Connect to the database
     db = MySQLdb.connect(
         host="localhost", user=username, passwd=password, db=database
     )
-    # queries to be run
+
+    # Create a cursor object
+    cur = db.cursor()
+    # Execute the query
     query = "SELECT cities.name\
             FROM cities JOIN states\
             ON cities.state_id = states.id WHERE states.name = %s"
-    conn = db.conn()
-    conn.execute(query, (state_name, ))
-    cities = [city[0] for city in conn.fetchall()]
+    # Fetch and print results
+    cities = [city[0] for city in cur.fetchall()]
     print(", ".join(cities))
-    conn.close()
+
+    # Close cursor and database connection
+    cur.close()
     db.close()
