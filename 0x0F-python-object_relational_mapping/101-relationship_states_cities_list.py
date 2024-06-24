5c3845d3127475345cdcbfd7df7ab30fdba077ee
Stevecmd <murimi101@gmail.com>
2024-06-24 23:52:46 +0300

    101-relationship_states_cities_list.py update

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,44 +1,48 @@
 #!/usr/bin/python3
 """
-Lists all State objects and corresponding
-City objects contained in the database
-hbtn_0e_101_usa.
+Prints the State and City objects from the database.
 """
 import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, joinedload
 from relationship_state import Base, State
 from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, joinedload
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        print(f"Usage: {sys.argv[0]} <username> <password> <database>")
         sys.exit(1)
 
-    username, password, database = sys.argv[1:4]
-    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        username, password, database
+    # Database connection parameters
+    db_user = sys.argv[1]
+    db_password = sys.argv[2]
+    db_name = sys.argv[3]
+    db_connection_string = (
+        f'mysql+mysqldb://{db_user}:{db_password}@'
+        f'localhost:3306/{db_name}'
     )
-    engine = create_engine(db_uri, pool_pre_ping=True)
 
-    # Bind the engine to the Base class which defines the models
-    Base.metadata.bind = engine
+    # Create a database engine
+    engine = create_engine(db_connection_string)
 
-    # Create a configured "Session" class
+    # Create the required tables if they don't exist
+    Base.metadata.create_all(engine)
+
+    # Create a session
     Session = sessionmaker(bind=engine)
-
-    # Create a Session instance
     session = Session()
 
-    # Query all State objects and their cities, sorted by state id and city id
-    states = session.query(State)\
-                    .options(joinedload(State.cities))\
-                    .order_by(State.id)\
-                    .all()
+    # Query and print State and associated City objects
+    states = (
+        session.query(State)
+        .options(joinedload(State.cities))
+        .order_by(State.id)
+        .all()
+    )
 
-    for state in states:
-        print("{}: {}".format(state.id, state.name))
-        for city in sorted(state.cities, key=lambda c: c.id):
-            print("\t{}: {}".format(city.id, city.name))
+    for state_instance in states:
+        print(f"{state_instance.id}: {state_instance.name}")
+        for city_instance in state_instance.cities:
+            print(f"    {city_instance.id}: {city_instance.name}")
 
     session.close()
