c75b9d42c6d9c2e45534a0f0378c870a11014ce5
Stevecmd <murimi101@gmail.com>
2024-06-30 00:47:37 +0300

    101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -18,3 +18,39 @@ def print_status_codes(size, status_codes):
         size (int): The accumulated read file size.
         status_codes (dict): The accumulated count of status codes.
     """
+    print("File size: {}".format(size))
+    for code in sorted(status_codes.keys()):
+        print("{}: {}".format(code, status_codes[code]))
+
+
+def main():
+    import sys
+
+    size = 0
+    status_codes = {}
+    line_count = 0
+
+    try:
+        for line in sys.stdin:
+            parts = line.split()
+            if len(parts) > 1:
+                size += int(parts[-1])
+                status_code = parts[-2]
+                if status_code.isdigit():
+                    status_codes[status_code] = \
+                        status_codes.get(status_code, 0) + 1
+
+            line_count += 1
+            if line_count % 10 == 0:
+                print_status_codes(size, status_codes)
+
+    except KeyboardInterrupt:
+        print_status_codes(size, status_codes)
+        raise
+
+    if line_count % 10 != 0:  # Print remaining lines if not a multiple of 10
+        print_status_codes(size, status_codes)
+
+
+if __name__ == "__main__":
+    main()
