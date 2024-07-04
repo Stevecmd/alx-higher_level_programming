2d86eedf089062ac9bbaf5bf1239c8c772ad9f03
Stevecmd <murimi101@gmail.com>
2024-07-04 13:15:05 +0300

    revert change and put proper first line

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
-#!/bin/bash#!/bin/bash
+#!/bin/bash
 # Sends a POST request to the passed URL with email and subject variables and displays the body of the response
-curl -s -X POST -d "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD" "$1"
+curl -sX POST -d email=test@gmail.com -d subject="I will always be here for PLD" "$1"
