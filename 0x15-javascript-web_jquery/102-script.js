92c9be95f2989850b3c3e1960c1cf0f336192a80
SteveCmd <murimi101@gmail.com>
2024-08-07 16:05:30 +0300

    updated link

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -2,7 +2,7 @@
 $(document).ready(function () {
   $('#btn_translate').click(function () {
     const langCode = $('#language_code').val();
-    const url = `https://www.fourtonfish.com/hellosalut/hello/?lang=${langCode}`;
+    const url = `https://hellosalut.stefanbohacek.dev/?lang=${langCode}`;
 
     $.get(url, function (data) {
       $('#hello').text(data.hello);
